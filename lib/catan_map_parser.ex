08a24872f0d6ff85214850ac29913cbbaa79cb9c
Tim Mecklem <tim@teamgaslight.com>
2016-10-31 21:41:19 -0400

    Add road parsing

--- lib/catan_map_parser.ex
+++ lib/catan_map_parser.ex
@@ -44,18 +44,37 @@ defmodule CatanMapParser do
 
   defp map_edges(board, map_lines) do
     edges = Enum.reduce(board.tiles, %{}, fn({location, _}, edges) ->
-      hex_to_ascii(location, origin_and_boundaries(map_lines))
+      hex_ascii_center = hex_to_ascii(location, origin_and_boundaries(map_lines))
+
+      edges = hex_ascii_center
       |> HexParser.parse_harbors(map_lines)
       |> merge_harbors(location, edges)
+
+      hex_ascii_center
+      |> HexParser.parse_roads(map_lines)
+      |> merge_roads(location, edges)
     end)
     Map.put_new(board, :edges, edges)
   end
 
   defp merge_harbors(hex_harbors, location, board_edges) do
     Enum.reduce(hex_harbors, board_edges, fn({direction, harbor_resource}, board_edges) ->
-      normalized_location = Hexagon.edge(location, direction)
-      Map.put(board_edges, normalized_location, %{harbor_resource: harbor_resource})
+      update_edge(board_edges, Hexagon.edge(location, direction), %{harbor_resource: harbor_resource})
     end)
+  end
+
+  defp merge_roads(roads, location, board_edges) do
+    Enum.reduce(roads, board_edges, fn({direction, player_road}, _board_edges) ->
+      update_edge(board_edges, Hexagon.edge(location, direction), %{player: player_road})
+    end)
+  end
+
+  defp update_edge(board_edges, location, information) do
+    new_value = case board_edges[location] do
+                  nil -> information
+                  value = %{} -> Map.merge(value, information)
+                end
+    Map.put(board_edges, location, new_value)
   end
 
   defp add_tile(nil, _, tiles), do: tiles
--- lib/hex_parser.ex
+++ lib/hex_parser.ex
@@ -54,7 +54,33 @@ defmodule HexParser do
   defp parse_harbor(_), do: nil
 
   defp add_harbor(nil, _, edges), do: edges
-  defp add_harbor(resource_type, direction, edges), do: Map.put_new(edges, direction, resource_type)
+  defp add_harbor(resource_type, direction, edges) do
+    Map.put_new(edges, direction, resource_type)
+  end
+
+  @road_offsets %{
+    se: { 5,  1}, s: {0,  3}, sw: {-5,  1}
+  }
+  def parse_roads(l = %AsciiLocation{}, map_lines) do
+    Enum.reduce(@road_offsets, %{}, fn({direction, {delta_x, delta_y}}, edges) ->
+      road_location = %AsciiLocation{x: l.x + delta_x, y: l.y + delta_y}
+      Enum.at(map_lines, road_location.y)
+      |> String.at(road_location.x)
+      |> parse_road
+      |> add_road(direction, edges)
+    end)
+  end
+
+  defp parse_road("b"), do: :blue
+  defp parse_road("o"), do: :orange
+  defp parse_road("r"), do: :red
+  defp parse_road("w"), do: :white
+  defp parse_road(_), do: nil
+
+  defp add_road(nil, _, edges), do: edges
+  defp add_road(player, direction, edges) do
+    Map.put_new(edges, direction, player)
+  end
 
   defp contains_robber?(map_lines, location = %AsciiLocation{}) do
     Enum.at(map_lines, location.y + 1) |> String.at(location.x) == "B"
